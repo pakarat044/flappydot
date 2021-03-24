804b944a11247f7bd44e70645c062413e3398f3b
vnsvakanda <phinyaphak.c@ku.th>
2021-03-25 04:08:16 +0700

    Resolve #1

--- flappydot.py
+++ flappydot.py
@@ -7,11 +7,19 @@ CANVAS_HEIGHT = 500
 
 UPDATE_DELAY = 33
 GRAVITY = 2.5
+STARTING_VELOCITY = -30
 
 class Dot(Sprite):
-    pass
 
+    def init_element(self):
+        self.vy = STARTING_VELOCITY
+        self.vy = -30
 
+    def update(self):
+        self.y += self.vy
+        self.vy += GRAVITY
+
+    
 class FlappyGame(GameApp):
     def create_sprites(self):
         self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
@@ -33,7 +41,7 @@ class FlappyGame(GameApp):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Monkey Banana Game")
+    root.title("Flappy Game")
  
     # do not allow window resizing
     root.resizable(False, False)
